9fa3c7c8c01b704a5e9930f1e1a49c643c262c3f
abbymccarthy <68303036+abbymccarthy@users.noreply.github.com>
2020-08-11 16:35:02 -0400

    Add files via upload

--- KAWDG01.py
+++ KAWDG01.py
@@ -1,91 +1,117 @@
 import numpy as np
 import pandas as pd
-import datetime
+import datetime as dt
 import csv
 import matplotlib.pyplot as plt
+import os
+
+# .csv files headers = ['Date','Water Levels (m)']
+
 import matplotlib.dates as mdates
-headers = ['Date','Water Levels (m)']
+from matplotlib.dates import date2num , DateFormatter
 
-# field measurements
-df = pd.read_csv('Field_measurements_m.CSV',names=headers)
-# x axis values
-x = df['Date']
-# y axis values
-y = df['Water Levels (m)']
-# plot
-plt.plot(x, y, color='green') 
-# naming the y axis 
+# point code to the data.
+cwd = os.chdir("/Users/abigailmccarthy/Desktop/the real desktop/thesis/KAW-DG-01_content/") 
+
+field_data = pd.read_excel('Field_measurements.xlsx', parse_dates = ['Date'])
+
+# figure handle to change properties more easily
+field_data_fig = plt.figure(figsize = (15, 6))
+
+plt.plot(field_data['Date'], field_data['Water Levels (m)'], color='green') 
+
+# naming the y axis, giving title to graph
 plt.ylabel('Water Levels (m)')
-# giving a title to my graph 
 plt.title('Field Measurements')
-#limiting number of ticks displayed on x axis <-- Apparently needs something?
-ax = df.plot(x_compat=True)
-ax.xaxis.set_major_locator(mdates.MonthLocator())
-# function to show the plot
+
+field_data_fig.tight_layout() # <-- extends plot to the border of the figure window
+# field_data_fig.savefig('Field Data.png', dpi = 300) # <-- to save figures as a .png file
+
 plt.show()
 
-# KGS Index Well
-df = pd.read_csv('KGS_Index_Well_m.CSV',names=headers)
-# x axis values
-x = df['Date']
-# y axis values
-y = df['Water Levels (m)']
-# plot
-plt.plot(x, y, color='purple') 
+
+#%% KGS Index Well Plot
+kgs_data = pd.read_csv('KGS_Index_Well.CSV', parse_dates = ['Date'])
+
+kgs_data_fig = plt.figure(figsize = (15,6))
+
+plt.plot(kgs_data['Date'], kgs_data['Water Levels (m)'], color='purple') 
+
 plt.ylabel('Water Levels (m)')
-# giving a title to my graph 
 plt.title('KGS Index Well')
-#limiting number of ticks displayed on x axis
-plt.MaxNLocator(10)
-# function to show the plot
+plt.xticks(rotation = 90)
+
 plt.show()
 
-# USGS Daily Data
-df = pd.read_csv('USGS_Daily_Data_m.CSV',names=headers)
-# x axis values
-x = df['Date']
-# y axis values
-y = df['Water Levels (m)']
-# plot
-plt.plot(x, y)
-# naming the axis   
+
+#%% USGS Data Plot
+usgs_data = pd.read_csv('USGS_Daily_Data.CSV', parse_dates=['Date'])
+
+usgs_data_fig = plt.figure(figsize = (15,6))
+
+
+plt.plot(usgs_data['Date'], usgs_data['Water Levels (m)']) 
+
 plt.ylabel('Water Levels (m)')
-# giving a title to my graph 
 plt.title('USGS Daily Data')
-#limiting number of ticks displayed on x axis
-plt.MaxNLocator(10)
-# function to show the plot
+
 plt.show()
 
+
+#%%
+#stack three plots, (#rows,#columns,plot#)
+plt.style.use('fivethirtyeight')
+fig = plt.figure()
+plt1 = fig.add_subplot(3,1,1)
+plt2 = fig.add_subplot(3,1,2)
+plt3 = fig.add_subplot(3,1,3)
+
+field_data = pd.read_excel('Field_measurements.xlsx', parse_dates = ['Date'])
+x1 = field_data['Date']
+y1 = field_data['Water Levels (m)']
+plt1.plot(x1, y1, color ='green')
+plt1.set_title('Field Measurements')
+
+kgs_data = pd.read_csv('KGS_Index_Well.CSV', parse_dates = ['Date'])
+x2 = kgs_data['Date']
+y2 = kgs_data['Water Levels (m)']
+plt2.plot(x2, y2, color ='blue')
+plt2.set_title('Index Well')
+
+usgs_data = pd.read_csv('USGS_Daily_Data.CSV', parse_dates=['Date'])
+x3 = usgs_data['Date']
+y3 = usgs_data['Water Levels (m)']
+plt3.plot(x3, y3, color ='purple')
+plt3.set_title('USGS Daily Data')
+fig.subplots_adjust(hspace=1,wspace=1)
+plt.show()
+
+#%%
 #All lines on the same graph <-- looks weird right now
-df = pd.read_csv('Field_measurements_m.CSV',names=headers)
-x1 = df['Date']
-y1 = df['Water Levels (m)']
-plt.plot(x1, y1, color='green', label = "Field Measurements")
-# line 2 points 
-df = pd.read_csv('KGS_Index_Well_m.CSV',names=headers)
-x2 = df['Date']
-y2 = df['Water Levels (m)']
-plt.plot(x2, y2, color='purple', label = "KGS Index Well")
-# line 3 points 
-df = pd.read_csv('USGS_Daily_Data_m.CSV',names=headers)
-x3 = df['Date']
-y3 = df['Water Levels (m)']
-plt.plot(x3, y3, label = "USGS Daily Data")
+
+field_data = pd.read_excel('Field_measurements.xlsx', parse_dates = ['Date'])
+x1 = field_data['Date']
+y1 = field_data['Water Levels (m)']
+plt.plot(x1, y1, color ='green', label = "Field Measurements")
+
+kgs_data = pd.read_csv('KGS_Index_Well.CSV', parse_dates = ['Date'])
+x2 = kgs_data['Date']
+y2 = kgs_data['Water Levels (m)']
+plt.plot(x2, y2, color ='blue', label = "KGS Index Well")
+
+usgs_data = pd.read_csv('USGS_Daily_Data.CSV', parse_dates=['Date'])
+x3 = usgs_data['Date']
+y3 = usgs_data['Water Levels (m)']
+plt.plot(x3, y3, color ='purple', label = "USGS Daily Data")
+plt.show()
+
 # naming the y axis 
 plt.ylabel('Water Levels (m)')
 # giving a title to my graph 
 plt.title('Well Data')
 #limiting number of ticks displayed on x axis <-- Apparently needs something?
 plt.MaxNLocator(10)
-# plot legend
 plt.legend() 
-# function to show the plot
-plt.show()
-
-df.plot(figsize=(15,4))
-plt.show()
-df.plot(subplots=True, figsize=(15,6))
 plt.show()
 
 
